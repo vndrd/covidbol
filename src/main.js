e58da8c049a0406f356d1871b4d36869b93b4a22
vndrd <ivangddzzz@gmail.com>
2020-06-10 12:45:18 -0400

    reading local file

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+  Vue.use(BootstrapVue)
+  // Optionally install the BootstrapVue icon components plugin
+  Vue.use(IconsPlugin)
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import HighchartsVue from 'highcharts-vue'
+Vue.use(HighchartsVue)
 
 Vue.config.productionTip = false
 
